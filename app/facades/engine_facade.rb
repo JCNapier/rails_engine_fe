301c8908032be0d0cf210acd608b181cd8803d1d
John Napier <jcee.napier@gmail.com>
2022-02-14 14:12:21 -0700

    Add facade method for merchants

--- app/facades/engine_facade.rb
+++ app/facades/engine_facade.rb
@@ -1,4 +1,12 @@
 class EngineFacade
+  def self.merchants 
+    json = EngineService.merchants 
+
+    json.map do |data|
+      Merchant.new(data) 
+    end
+  end
+
   def self.merchant_items(merchant_id)
     json = EngineService.merchant_items(merchant_id)
 
