0571521993477f9ee54f40d3d2fe9b00d7319753
John Napier <jcee.napier@gmail.com>
2022-02-14 14:51:30 -0700

    Add merchant name to show page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,7 +4,7 @@ class MerchantsController < ApplicationController
   end
 
   def show 
-    # require 'pry'; binding.pry
+    @merchant = EngineFacade.merchants.find { |m| m.id == (params[:id]) }
     @merchant_items = EngineFacade.merchant_items(params[:id])
   end
 end
